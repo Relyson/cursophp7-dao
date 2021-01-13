2c23d50d987663e7680870216bb106b756af2221
Relyson Ramos <relyson.l.m.ramos@gmail.com>
2021-01-13 11:24:40 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -2,14 +2,27 @@
 
 require_once("config.php");
 
-$user = new Usuario();
+//Carrega um usuário
+//$user = new Usuario();
+//$user->loadbyId(3);
+//echo $user;
 
-$user->loadbyId(3);
+//Carregar uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista); 
 
-echo $user;
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("A");
+//echo json_encode($search);
+
+//Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("user", "12345");
+
+echo $usuario;
 
 
-/*
+/************************************
 $sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
